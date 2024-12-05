85f7e9237ecc026148a7c0678fc47ce2c27e8265
alaminsframe <helloalaminsheikh@gmail.com>
2024-12-06 01:33:59 +0600

    created some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './index.css'
+import SideBar from './components/side-bar';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>hello</p>
+      <SideBar />
     </>
   )
 }
---
+++ src/components/side-bar.jsx
@@ -0,0 +1,13 @@
+import { MdOutlineHome } from "react-icons/md";
+
+const SideBar = () => {
+  return (
+    <>
+         <MdOutlineHome size={40} className='bg-red-500 rounded-full p-1' fill='bule'/>
+        <p>hello world</p>
+    
+    </>
+  )
+}
+
+export default SideBar
